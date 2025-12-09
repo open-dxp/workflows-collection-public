53d814933e23f67aface68b2070d67a558313758
Stefan Hagspiel <shagspiel@dachcom.ch>
2025-12-09 12:23:57 +0100

    Update PHP-CS-Fixer configuration with additional rules and improved formatting consistency

--- php-cs-fixer-configuration/.php-cs-fixer.dist.php
+++ php-cs-fixer-configuration/.php-cs-fixer.dist.php
@@ -35,7 +35,9 @@ $config->setRules([
     'single_line_comment_style'           => true,
     'lowercase_cast'                      => true,
     'magic_constant_casing'               => true,
-    'method_argument_space'               => ['on_multiline' => 'ignore'],
+    'method_argument_space'               => [
+        'on_multiline' => 'ignore'
+    ],
     'class_attributes_separation'         => true,
     'native_function_casing'              => true,
     'no_blank_lines_after_class_opening'  => true,
@@ -48,7 +50,10 @@ $config->setRules([
     'no_leading_namespace_whitespace'     => true,
     'no_short_bool_cast'                  => true,
     'no_spaces_around_offset'             => true,
-    'no_superfluous_phpdoc_tags'          => ['allow_mixed' => true, 'remove_inheritdoc' => true],
+    'no_superfluous_phpdoc_tags'          => [
+        'allow_mixed'       => true,
+        'remove_inheritdoc' => true
+    ],
     'no_unneeded_control_parentheses'     => true,
     'no_unused_imports'                   => true,
     'no_whitespace_before_comma_in_array' => true,
@@ -59,6 +64,7 @@ $config->setRules([
     'phpdoc_scalar'                       => true,
     'phpdoc_separation'                   => true,
     'phpdoc_single_line_var_spacing'      => true,
+    'phpdoc_trim'                         => true,
     'return_type_declaration'             => true,
     'short_scalar_cast'                   => true,
     'single_blank_line_before_namespace'  => true,
@@ -68,14 +74,18 @@ $config->setRules([
     'ternary_operator_spaces'             => true,
     'trailing_comma_in_multiline'         => true,
     'whitespace_after_comma_in_array'     => true,
-    'global_namespace_import' => [
-        'import_classes' => true,
+    'multiline_promoted_properties'       => [
+        'minimum_number_of_parameters' => 3
+    ],
+    'global_namespace_import'             => [
+        'import_classes'   => true,
         'import_constants' => true,
         'import_functions' => true,
     ],
-    'ordered_imports' => [
+    'ordered_imports'                     => [
         'imports_order' => ['const', 'class', 'function']
     ],
+    'ordered_attributes'                  => true
 ]);
 
 $config->setFinder($finder);
