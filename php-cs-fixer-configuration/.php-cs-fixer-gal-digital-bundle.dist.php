e0421386d9d55e377e815600261c6835b6f78e15
Stefan Hagspiel <shagspiel@dachcom.ch>
2026-07-14 11:14:25 +0200

    add GAL Digital bundle configuration to PHP-CS-Fixer setup and workflows

---
+++ php-cs-fixer-configuration/.php-cs-fixer-gal-digital-bundle.dist.php
@@ -0,0 +1,24 @@
+<?php
+
+$rulesFile = __DIR__ . '/.php-cs-fixer-rules-global.php';
+$finderFile = __DIR__ . '/.php-cs-fixer-finder.dist.php';
+$forkHeaderFile = __DIR__ . '/.php-cs-fixer-fork-header-global.php';
+
+if (!file_exists($finderFile)) {
+    throw new RuntimeException('Finder file not found: ' . $finderFile);
+}
+
+$finder = require $finderFile;
+$rules = require $rulesFile;
+$forkHeader = require $forkHeaderFile;
+
+$rules['header_comment'] = [
+    'comment_type' => 'PHPDoc',
+    'header'       => $forkHeader($finder, '@copyright  Copyright (c) GAL Digital GmbH (https://www.gal-digital.de)'),
+];
+
+$config = new PhpCsFixer\Config();
+$config->setRules($rules);
+$config->setFinder($finder);
+
+return $config;
